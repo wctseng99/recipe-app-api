89bfdf07354523f2725537a3eac3b6a81df56d00
wei-chun tseng <wctseng99@gmail.com>
2024-04-22 15:24:54 +0800

    test: add test for email address validation

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,8 @@ class ModelTests(TestCase):
         for email, expected in sample_email:
             user = get_user_model().objects.create_user(email, "test123")
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raise_error(self):
+        """Test creating user without email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123")
