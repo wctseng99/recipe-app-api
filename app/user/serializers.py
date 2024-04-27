d229a0b4950add893abed88a33165598c048a32a
wei-chun tseng <wctseng99@gmail.com>
2024-04-27 15:18:43 +0800

    build: update dependencies version

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth import (
     get_user_model,
     authenticate,
 )
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 from rest_framework import serializers
 
